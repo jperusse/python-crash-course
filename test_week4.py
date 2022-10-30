0874071675f8f4b043459d5e705b0cb62a73e221
Jim Perusse <67986763+jperusse@users.noreply.github.com>
2022-10-29 21:20:06 -0700

    Use f-string for formatting

--- test_week4.py
+++ test_week4.py
@@ -5,11 +5,11 @@ from week4 import format_address
 class TestModule4GradedAssignment(unittest.TestCase):
     """ Week4 test class """
     def test_format_address(self):
-        
+        """ Verify format_address """
         self.assertEqual(format_address("123 Main Street"),
                          '"house number 123 on street named Main Street"')
 
-        self.assertEqual(format_address("1001 1st Ave"), 
+        self.assertEqual(format_address("1001 1st Ave"),
                          '"house number 1001 on street named 1st Ave"')
 
         self.assertEqual(format_address("55 North Center Drive"),
--- week4.py
+++ week4.py
@@ -1,24 +1,27 @@
 """ Week4 package with all sample code """
 
 def format_address(address_string):
-  # Declare variables
-  house_number = ""
-  street_name = ""
+    """
+        The format_address function separates out parts of the address string into new strings: house_number and street_name, and returns: "house number X on street named Y". The format of the input string is: numeric house number, followed by the street name which may contain numbers, but never by themselves, and could be several words long. For example, "123 Main Street", "1001 1st Ave", or "55 North Center Drive". Fill in the gaps to complete this function.
+    """
+    # Declare variables
+    house_number = ""
+    street_name = ""
 
-  # Separate the address string into parts
-  address_parts = address_string.split()
+    # Separate the address string into parts
+    address_parts = address_string.split()
 
-  # Traverse through the address parts
-  for part in address_parts:
-    # Determine if the address part is the
-    # house number or part of the street name
-    if part.isnumeric() :
-      house_number = part
-    else:
-      street_name += part + " "
-    
-  # Does anything else need to be done 
-  # before returning the result?
-  
-  # Return the formatted string  
-  return '"house number {} on street named {}"'.format(house_number, street_name.rstrip())
+    # Traverse through the address parts
+    for part in address_parts:
+        # Determine if the address part is the
+        # house number or part of the street name
+        if part.isnumeric() :
+            house_number = part
+        else:
+            street_name += part + " "
+
+    # Does anything else need to be done
+    # before returning the result?
+
+    # Return the formatted string
+    return f'"house number {house_number} on street named {street_name.rstrip()}"'
