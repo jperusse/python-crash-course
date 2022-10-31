09ae5a58a5e0d52ae3e0a741ef23c12cbf61ea6a
Jim Perusse <67986763+jperusse@users.noreply.github.com>
2022-10-31 13:21:44 -0700

    Exercise 2

--- test_week4.py
+++ test_week4.py
@@ -1,8 +1,9 @@
 """ week4 test class and methods """
 import unittest
-from week4 import format_address
+from week4 import format_address, highlight_word
 
-class TestModule4GradedAssignment(unittest.TestCase):
+
+class TestWeek4(unittest.TestCase):
     """ Week4 test class """
 
     def test_format_address(self):
@@ -15,3 +16,12 @@ class TestModule4GradedAssignment(unittest.TestCase):
 
         self.assertEqual(format_address("55 North Center Drive"),
                          '"house number 55 on street named North Center Drive"')
+
+    def test_highlight_word(self):
+        """ Use all CAPS to highlight a word in a sentence """
+        self.assertEqual(highlight_word("Have a nice day", "nice"),
+                         "Have a NICE day")
+        self.assertEqual(highlight_word("Shhh, don't be so loud!", "loud"),
+                         "Shhh, don't be so LOUD!")
+        self.assertEqual(highlight_word("Automating with Python is fun", "fun"),
+                         "Automating with Python is FUN")
--- week4.py
+++ week4.py
@@ -1,5 +1,6 @@
 """ Week4 package with all sample code """
 
+# Week 4
 def format_address(address_string):
     """
         The format_address function separates out parts of the address string into new strings: house_number and street_name, and returns: "house number X on street named Y". The format of the input string is: numeric house number, followed by the street name which may contain numbers, but never by themselves, and could be several words long. For example, "123 Main Street", "1001 1st Ave", or "55 North Center Drive". Fill in the gaps to complete this function.
@@ -25,3 +26,16 @@ def format_address(address_string):
 
     # Return the formatted string
     return f'"house number {house_number} on street named {street_name.rstrip()}"'
+
+def highlight_word(sentence, word):
+    """
+    The highlight_word function changes the given word in a sentence to its upper-case version. For example, highlight_word("Have a nice day", "nice") returns "Have a NICE day". Can you write this function in just one line?
+    """
+    new_list = []
+    for w in sentence.split():
+        if word in w:
+            new_list.append(w.upper())
+        else:
+            new_list.append(w)
+    new_sentence = " ".join(new_list)
+    return new_sentence
